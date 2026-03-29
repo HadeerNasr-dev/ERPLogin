22c0c7525549e5556278588fe6f4714d78d8d7d5
Hadeer_Nasr <hadeernasr62000@gmail.com>
2026-03-29 02:02:57 +0200

    git config --global user.email "hadeernasr62000@gmail.com"

---
+++ src/test/java/testlogin/LoginTests.java
@@ -0,0 +1,64 @@
+package testlogin;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.mapper.ObjectMapperType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class LoginTests {
+    @BeforeClass
+    public void setup() {
+        // Base URI for ERP
+        RestAssured.baseURI = "http://69.10.56.98/backend";
+    }
+
+    @Test(description = "Verify successful login with valid credentials")
+    public void testSuccessfulLogin() {
+        LoginData validUser = new LoginData( "gepid57587@besenica.com", "Asd@123456");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(validUser, ObjectMapperType.JACKSON_2) // Jackson serializes this POJO automatically
+                .when()
+                .post("/api/Authenticate/Login")
+                .then()
+                .statusCode(200) // Success
+                .body("token", notNullValue()) // Ensure a token is returned
+                .body("message", equalTo("Login successful"))
+                .log().all();
+    }
+
+    @Test(description = "Verify login failure with incorrect password")
+    public void testLoginWithWrongPassword() {
+        LoginData invalidUser = new LoginData("gepid57587@besenica.com", "468676");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(invalidUser).log().all()
+                .when()
+                .post("/api/Authenticate/Login")
+                .then()
+                .statusCode(400) // Unauthorized
+                .log().all();
+    }
+
+    @Test(description = "Verify login failure with non-existent user")
+    public void testLoginWithInvalidUser() {
+        LoginData ghostUser = new LoginData("gepid@besenica.com", "Asd@123456");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(ghostUser)
+                .when()
+                .post("/api/Authenticate/Login")
+                .then()
+                .statusCode(400) // Not Found
+                .log().all();
+    }
+}
+
